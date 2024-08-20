d8d6272545613f48191f7409639074ff40d4e7f3
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-20 17:54:41 +0100

    completed collections page

--- app/Hero/Collections.jsx
+++ app/Hero/Collections.jsx
@@ -1,1 +1,23 @@
-rfce;
+import React from "react";
+
+const Container = () => {
+  return (
+    <section className=" w-auto border flex justify-between items-center rounded-[20px] flex-col h-[218px] ">
+      <p className="uppercase text-[32px] font-semibold w-full text-center mt-6 ">
+        Title study
+      </p>
+      <div className="h-[119px] bg-black w-full rounded-[10px] "></div>
+    </section>
+  );
+};
+function Collections() {
+  return (
+    <main className=" w-full grid mt-[132px] mb-[41px] grid-cols-2 sm:grid-cols-4 gap-8 ">
+      {[1, 2, 3, 4].map((d, id) => {
+        return <Container key={id} />;
+      })}
+    </main>
+  );
+}
+
+export default Collections;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./Hero/Hero";
+import Collections from "./Hero/Collections";
 
 export default function Home() {
   return (
-    <main className="overflow-x-hidden">
+    <main className="overflow-x-hidden px-4">
       <Hero />
+      <Collections />
     </main>
   );
 }
