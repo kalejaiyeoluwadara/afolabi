794fea5ab9577a739bc039d56ea9dd3e82e36044
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-25 15:48:52 +0100

    fixed container

--- app/components/Container.jsx
+++ app/components/Container.jsx
@@ -5,61 +5,23 @@ import { images } from "@/app/utils";
 import Image from "next/image";
 import { gsap } from "gsap";
 
-const Container = ({ name, link }) => {
-  const imageContainerRef = useRef(null);
-  const imageRef = useRef(null);
-
-  useEffect(() => {
-    const hoverAnimation = gsap.timeline({ paused: true });
-
-    hoverAnimation.to(imageContainerRef.current, {
-      top: "0px",
-      duration: 0.5,
-      ease: "power2.inOut", // Smooth, yet fast ease
-    });
-
-    hoverAnimation.to(
-      imageRef.current,
-      {
-        opacity: 1,
-        rotate: 0,
-        duration: 0.5, // Shorter duration for faster fade-in
-        ease: "power2.out",
-      },
-      0
-    );
-
-    const handleMouseEnter = () => hoverAnimation.play();
-    const handleMouseLeave = () => hoverAnimation.reverse();
-
-    const imageContainerElement = imageContainerRef.current;
-    imageContainerElement.addEventListener("mouseenter", handleMouseEnter);
-    imageContainerElement.addEventListener("mouseleave", handleMouseLeave);
-
-    return () => {
-      imageContainerElement.removeEventListener("mouseenter", handleMouseEnter);
-      imageContainerElement.removeEventListener("mouseleave", handleMouseLeave);
-    };
-  }, []);
-
+const Container = ({ name, link, icon }) => {
   return (
-    <Link href={"/"}>
-      <section className="w-auto border relative flex justify-between items-center rounded-[20px] flex-col h-[218px] overflow-hidden">
-        <p className="uppercase text-[32px] font-semibold w-full text-center mt-6">
-          {name}
-        </p>
-        <div
-          ref={imageContainerRef}
-          className="h-[218px] absolute top-[80px] flex-center cursor-pointer overflow-hidden bg-black w-full rounded-[10px]"
-        >
-          <Image
-            ref={imageRef}
-            src={images.flower}
-            alt=""
-            height={70}
-            width={70}
-            className=" opacity-0 rotate-180" // Start image as hidden
-          />
+    <Link className="group" href={"/"}>
+      <section className="w-auto border-2 border-dashed  border-black group-hover:border-none relative flex justify-between transition-all  delay-[200] items-center rounded-[20px] flex-col h-[218px] overflow-hidden">
+        <div className="relative w-full group-hover:-translate-y-[80px] transition-all ">
+          <p className="uppercase text-[32px]  font-semibold w-full text-center mt-6">
+            {name}
+          </p>
+          <div className="h-[218px] absolute top-[80px] flex-center cursor-pointer overflow-hidden bg-black w-full rounded-[10px]">
+            <Image
+              src={images.flower}
+              alt=""
+              height={70}
+              width={70}
+              className=" opacity-0 group-hover:opacity-[1] group-hover:rotate-180 transition-all " // Start image as hidden
+            />
+          </div>
         </div>
       </section>
     </Link>
