de7e75d9270e4e4eb78dd5ceef24baabb18d2b48
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-08 22:50:02 +0100

    footer resp

--- app/Hero/Foot.jsx
+++ app/Hero/Foot.jsx
@@ -7,28 +7,30 @@ import Image from "next/image";
 function Foot() {
   return (
     <footer
-      className={`min-h-[488px] rounded-[20px] text-white mt-[58px] mb-[39px] w-full bg-black px-7 py-9 flex  flex-col justify-between  `}
+      className={`sm:min-h-[488px]  rounded-[20px] text-white mt-[58px] mb-[39px] w-full bg-black px-7 py-9 flex sm:items-start flex-center flex-col justify-between  `}
     >
-      <div className="flex text-base font-normal flex-col mt-4 ">
-        <p className=" uppercase text-[24px] ">OYEFESO Selected Work</p>
-        <p>‘22 - ‘24</p>
+      <div className="flex items-start justify-normal flex-center text-base font-normal flex-col mt-4 ">
+        <p className=" uppercase text-base sm:text-[24px] ">
+          OYEFESO Selected Work
+        </p>
+        <p className="text-base sm:text-[24px]">‘22 - ‘24</p>
       </div>
       <div className="w-full flex-center ">
-        <h1 className="text-[59px] uppercase text-center leading-[80px] w-[507px] font-semibold">
+        <h1 className="sm:text-[59px] sm:my-0 my-[34px] text-5xl uppercase text-center sm:leading-[80px] sm:w-[507px] font-semibold">
           Interested in worKING together?
         </h1>
       </div>
-      <div className=" group w-full flex-col flex-center  mt-[49px] mb-[71px] ">
+      <div className=" group w-full flex-col flex-center  sm:mt-[49px] sm:mb-[71px] ">
         <p className="text-primary text-base group-hover:text-white  speed ">
           DROP AN EMAIL
         </p>
-        <p className="text-2xl group-hover:text-primary cursor-pointer speed underline uppercase ">
+        <p className="sm:text-2xl text-base group-hover:text-primary cursor-pointer speed underline uppercase ">
           Oyefesoafolabiteniola@gmail.com
         </p>
       </div>
-      <div className="flex justify-between items-center w-full  ">
+      <div className="flex sm:flex-row flex-col-reverse  justify-between items-center w-full  ">
         <p className="text-[#747474] text-base ">MY AHA MOMENT</p>
-        <div className="flex uppercase text-[30px] items-center gap-4 ">
+        <div className=" hidden sm:flex uppercase text-[30px] items-center gap-4 ">
           <Link href={"/"} className="hover:text-primary speed ">
             instagram
           </Link>
@@ -46,7 +48,7 @@ function Foot() {
           </Link>
         </div>
         <Link href={"#nav"}>
-          <div className="h-[60px] w-[60px] flex-center rounded-full group relative overflow-hidden bg-[#D9D9D9]  ">
+          <div className="h-[60px] sm:my-0 my-[39px] w-[60px] flex-center rounded-full group relative overflow-hidden bg-[#D9D9D9]  ">
             <div className="h-[85px] w-[85px] absolute  rounded-full top-20  group-hover:top-[-4px] group-hover:left-[-10px] bg-black speed " />
             <GrLinkUp
               size={20}
