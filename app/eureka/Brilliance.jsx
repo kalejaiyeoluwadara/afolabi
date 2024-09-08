b6811e133f04b652b3ff7ecabe9d6a25bebb1866
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-08 23:22:18 +0100

    brilliance resp

--- app/eureka/Brilliance.jsx
+++ app/eureka/Brilliance.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 function Brilliance() {
   return (
-    <main className="grid grid-cols-3 my-[30px] gap-[30px] h-[471px] w-full ">
-      <section className="thickb w-auto h-full flex-col flex-center ">
-        <h1 className="text-[64px] text-primary stratosBold font-bold ">
+    <main className="sm:grid grid-cols-3 flex flex-col-reverse my-[30px] gap-[30px] h-auto sm:h-[471px] w-full ">
+      <section className="thickb w-full sm:w-auto h-[310px]  sm:h-full flex-col flex-center ">
+        <h1 className=" text-6xl sm:text-[64px] text-primary stratosBold font-bold ">
           BRILLIANCE
         </h1>
-        <h1 className="text-[64px] text-black stratosBold font-bold ">
+        <h1 className=" text-6xl sm:text-[64px] text-black stratosBold font-bold ">
           BRILLIANCE
         </h1>
-        <h1 className="text-[64px] text-[#7F56D9] stratosBold font-bold ">
+        <h1 className=" text-6xl sm:text-[64px] text-[#7F56D9] stratosBold font-bold ">
           BRILLIANCE
         </h1>
       </section>
-      <section className="col-span-2 flex flex-col items-start justify-center px-[46px] gap-6 h-full thickb w-auto">
-        <h1 className="text-[64px] leading-[3.1rem]  stratosBold font-bold ">
+      <section className="sm:col-span-2 flex flex-col items-start justify-center py-[31px] px-[17px] sm:px-[46px] gap-6 h-full thickb w-auto">
+        <h1 className=" text-4xl sm:text-[64px] leading-[3.1rem]  stratosBold font-bold ">
           SPAWING NON-
         </h1>
-        <h1 className="text-[64px]  leading-[3.1rem] stratosBold font-bold ">
+        <h1 className=" text-4xl sm:text-[64px] sm:translate-y-0 translate-y-[-14px] sm:leading-[3.1rem] stratosBold font-bold ">
           STOP <span className="text-primary">CREATIVITY.</span>
         </h1>
-        <p className="font-clash text-[32px] ">
+        <p className="font-clash text-xl sm:translate-y-0 translate-y-[-20px] sm:text-[32px] ">
           Oyefeso here, a maverick of imagination on an unstoppable mission to
           shatter innovation barriers and harness the unbridled might of
           self-expression.
