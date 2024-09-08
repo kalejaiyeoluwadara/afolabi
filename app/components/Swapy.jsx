daa46d6fe5fe709da39b5d7163e6e46978a980a3
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-08 23:36:37 +0100

    swapy resp

--- app/components/Swapy.jsx
+++ app/components/Swapy.jsx
@@ -3,12 +3,12 @@ import { images } from "@/app/utils";
 import Image from "next/image";
 function Swapy() {
   return (
-    <>
+    <main className="sm:px-0 px-4">
       {/* Swapy 1 */}
-      <section className="grid grid-cols-2 border-y-[3px] border-black border-dashed w-full  overflow-hidden h-[384px] ">
-        <div className="h-full w-auto flex px-[61px] justify-center flex-col ">
+      <section className="sm:grid flex flex-col-reverse grid-cols-2 border-[3px] sm:border-y-[3px] border-black border-dashed w-full  overflow-hidden h-[430px] sm:rounded-none rounded-[20px] sm:h-[384px] ">
+        <div className="h-full w-full sm:w-auto flex px-[17px] sm:px-[61px] justify-center flex-col ">
           <h1>SWAPY</h1>
-          <p className=" text-[32px] font-clash sm:w-[99%] ">
+          <p className=" text-xl sm:text-3xl font-clash sm:w-[99%] ">
             This project showcases my creativity through thoughtful color
             choices, cohesive{" "}
           </p>
@@ -17,38 +17,38 @@ function Swapy() {
           <Image
             src={images.unito}
             alt=""
-            className="h-full absolute left-0 top-0 z-[10] w-full"
+            className="sm:h-full h-[290px] absolute left-0 top-0 z-[10] w-full"
           />
           <div className="w-full flex-center ">
-            <div className="absolute z-20 h-[115px]  w-auto px-[74px] py-[38px] flex-center wb rounded-[15px] group-hover:bottom-4 -bottom-[130px]  speed ">
+            <div className="absolute z-20 h-[90px] sm:h-[115px]  w-auto px-4 sm:px-[74px] py-3 sm:py-[38px] flex-center wb rounded-[15px] sm:group-hover:bottom-4 group-hover:-bottom-3 -bottom-[130px]  speed ">
               <h2 className="text-[32px]">CHECK ME OUT</h2>
             </div>
           </div>
         </div>
       </section>
       {/* Swapy 2*/}
-      <section className="grid mb-[124px] mt-[45px] grid-cols-2 border-y-[3px]  border-black border-dashed w-full overflow-hidden h-[384px] ">
-        <div className="h-full group w-auto overflow-hidden relative  scale-[1.2] ">
-          <Image
-            src={images.unito}
-            alt=""
-            className="h-full absolute left-0 top-0 z-[10] w-full"
-          />
-          <div className="w-full flex-center ">
-            <div className="absolute z-20 h-[115px]  w-auto px-[74px] py-[38px] flex-center wb rounded-[15px] group-hover:bottom-4 -bottom-[130px]  speed ">
-              <h2 className="text-[32px]">CHECK ME OUT</h2>
-            </div>
-          </div>
-        </div>
-        <div className="h-full w-auto flex px-[61px] justify-center items-end flex-col ">
+      <section className="sm:grid sm:mt-0 mt-8 flex flex-col-reverse grid-cols-2 border-[3px] sm:border-y-[3px] border-black border-dashed w-full  overflow-hidden h-[430px] sm:rounded-none rounded-[20px] sm:h-[384px] ">
+        <div className="h-full w-full sm:w-auto flex px-[17px] sm:px-[61px] justify-center flex-col ">
           <h1>SWAPY</h1>
-          <p className=" mt-3 font-clash text-end text-[32px] w-[99%] ">
+          <p className=" text-xl sm:text-3xl font-clash sm:w-[99%] ">
             This project showcases my creativity through thoughtful color
             choices, cohesive{" "}
           </p>
         </div>
+        <div className="h-full w-auto overflow-hidden relative group scale-[1.2] ">
+          <Image
+            src={images.unito}
+            alt=""
+            className="sm:h-full h-[290px] absolute left-0 top-0 z-[10] w-full"
+          />
+          <div className="w-full flex-center ">
+            <div className="absolute z-20 h-[90px] sm:h-[115px]  w-auto px-4 sm:px-[74px] py-3 sm:py-[38px] flex-center wb rounded-[15px] sm:group-hover:bottom-4 group-hover:-bottom-3 -bottom-[130px]  speed ">
+              <h2 className="text-[32px]">CHECK ME OUT</h2>
+            </div>
+          </div>
+        </div>
       </section>
-    </>
+    </main>
   );
 }
 
